148487c5e8ae6ee6e18e20307c0ef94ffed104ac
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-05-01 10:19:10 -0500

    loops-css

---
+++ loops.js
@@ -0,0 +1,26 @@
+// 1. Create a loop that logs the numbers from 0-99 (ascending)
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+  }
+
+// 2. Create a loop that logs the EVEN numbers from 0-98 (ascending)
+for (let i = 0; i <= 98; i += 2) {
+    console.log(i);
+  }
+
+// 3. Create a loop that logs the ODD numbers from 0-99 (ascending)
+for (let i = 1; i <= 99; i += 2) {
+    console.log(i);
+  }
+
+// 4. Create a loop that logs the numbers from 49-72 (ascending)
+for (let i = 49; i <= 72; i++) {
+    console.log(i);
+  }
+
+// 5. Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+for (let i = 3; i <= 90; i++) {
+    if (i % 3 === 0) {
+      console.log(i);
+    }
+  }
